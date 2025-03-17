8744f5f885e66228573a925bc2cb83b180b3a7f0
Thiago Pereira <tpgoncalvesc@gmail.com>
2025-03-17 17:52:16 -0300

    updating documentation

--- data_acquisition/get_stock_data.py
+++ data_acquisition/get_stock_data.py
@@ -2,7 +2,7 @@ import yfinance as yf
 
 def get_stock_data(ticker: str, period: str, interval: str):
     """
-    Fetches intraday stock data for the given ticket
+    Fetches stock data for the given ticket, period and interval
     """
     data = yf.Ticker(ticker).history(period=period, interval=interval)
     return data 
