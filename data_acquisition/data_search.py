bd90b14948db30c7606e3dc01e922882420556bd
Thiago Pereira <tpgoncalvesc@gmail.com>
2025-03-17 17:53:06 -0300

    updating documentation

--- data_acquisition/data_search.py
+++ data_acquisition/data_search.py
@@ -3,6 +3,7 @@ import requests
 def search_stock(company_name: str):
     """
     Search for stock ticker by company name using Yahoo Finance API.
+    Returns suggestion ticker for the given company name.
     """
     url=f"https://query2.finance.yahoo.com/v1/finance/search?q={company_name}"
     headers = {"User-agent": "Mozilla/5.0"}
