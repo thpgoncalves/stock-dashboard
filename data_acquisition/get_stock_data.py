930c55c3b2754d2561e1886240451f0b9b1bbd90
Thiago Pereira <tpgoncalvesc@gmail.com>
2025-03-17 17:55:05 -0300

    Organizing functions

--- data_acquisition/get_stock_data.py
+++ data_acquisition/get_stock_data.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+import pandas as pd
 
 def get_stock_data(ticker: str, period: str, interval: str):
     """
@@ -6,3 +7,36 @@ def get_stock_data(ticker: str, period: str, interval: str):
     """
     data = yf.Ticker(ticker).history(period=period, interval=interval)
     return data 
+
+def get_comparison_data(tickers: str | list[str], period="1mo", interval="1d"):
+    """
+    Fetch historical data for multiple tickers and normalize to a common start date.
+    """
+    df = pd.DataFrame()
+
+    for ticker in tickers:
+        try:
+            stock = yf.Ticker(ticker)
+            stock_data = stock.history(period=period, interval=interval)['Close']
+
+            if not stock_data.empty:
+                df[ticker] = stock_data
+        except Exception as e:
+            print(f"Failed to fetch data for {ticker}: {e}")
+
+    if df.empty:
+        return df
+    
+    # normalizing to a common start date
+    df = df.dropna(how="any")
+    valid_stocks = df.columns[df.iloc[0] > 0]
+    df = df[valid_stocks]
+
+    if df.empty:
+        print("No valid stocks found after filtering. Returning empty DataFrame.")
+        return pd.DataFrame()
+    
+    # calculating percent change
+    df = ((df / df.iloc[0]) - 1) * 100
+    
+    return df
--- data_processing/charts.py
+++ data_processing/charts.py
@@ -10,39 +10,6 @@ def create_chart(data: pd.DataFrame, title: str) -> pd.DataFrame:
     fig = px.line(data, x=data.index, y="Close", title=title)
     return fig
 
-def get_comparison_data(tickers: str | list[str], period="1mo", interval="1d"):
-    """
-    Fetch historical data for multiple tickers and normalize to a common start date.
-    """
-    df = pd.DataFrame()
-
-    for ticker in tickers:
-        try:
-            stock = yf.Ticker(ticker)
-            stock_data = stock.history(period=period, interval=interval)['Close']
-
-            if not stock_data.empty:
-                df[ticker] = stock_data
-        except Exception as e:
-            print(f"Failed to fetch data for {ticker}: {e}")
-
-    if df.empty:
-        return df
-    
-    # normalizing to a common start date
-    df = df.dropna(how="any")
-    valid_stocks = df.columns[df.iloc[0] > 0]
-    df = df[valid_stocks]
-
-    if df.empty:
-        print("No valid stocks found after filtering. Returning empty DataFrame.")
-        return pd.DataFrame()
-    
-    # calculating percent change
-    df = ((df / df.iloc[0]) - 1) * 100
-    
-    return df
-
 def plot_comparison_chart(df):
     """
     Generate comparison line chart for multiple stocks.
